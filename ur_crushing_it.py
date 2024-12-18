b710cf5276c038f7aaf709722bc3c8385339857f
DeanEby <ebyd21@gmail.com>
2024-12-18 17:05:27 -0500

    Improve debt calculation

--- ur_crushing_it.py
+++ ur_crushing_it.py
@@ -14,10 +14,12 @@ import pandas as pd
 savings_goal = 5000
 snp500_estimated_performance = .08
 # How many years do we simulate
-PERIOD = 20
-current_year = 0
+PERIOD = 6
+# how much +- are % are we introducing to find optimal budget
+WIGGLE = 0
+#current_year = 0
 
-bank = 0
+bank = open("secrets/bank.txt")
 investment_portfolio = []
 #debt_portfolio = {}
 
@@ -49,6 +51,11 @@ standard_budgeting_percentage_dict = {"housing": 30, # need
                             "debt": 10, # savings
                             "entertainment": 10, # want
                             "personal": 10} # want
+
+# for now we only need to know debt % and investments %
+real_budgeting_percentage_dict = {"debt": 10,
+                                  "investments": 10}
+
 
 
 budgeting_percentage_dict_values = list(standard_budgeting_percentage_dict.values())
@@ -114,7 +121,7 @@ smdb_string = dict_to_string(standard_monthly_dollar_budget)
 #print("Standard Budget for %s monthly salary is:" % monthly_salary, smdb_string)
 
 
-    # Current algorithm does not account ffor 
+    # Current algorithm does not account for minimum required payments
 def calculate_debt_after_period(period, debt_df, expenses_dict):
     # convert budget % to dollars
     dollar_dict = calculate_dollars_from_percentage(monthly_salary, expenses_dict)
@@ -122,7 +129,7 @@ def calculate_debt_after_period(period, debt_df, expenses_dict):
     loan_count = len(debt_df)
     # get the amount of money budgeted for paying off debt
     debt_monthly_budgeted = dollar_dict["debt"]
-    #print(debt_df)
+
     
     for year in range(period):
 
@@ -182,7 +189,102 @@ def calculate_debt_after_period(period, debt_df, expenses_dict):
 
     return # need to design an output for this
 
-##print(debt_df)
-debt_calculation = calculate_debt_after_period(PERIOD, debt_df, standard_budgeting_percentage_dict)
-#print(debt_calculation)
+def pay_debt(debt_df, amount):
+    # dictionary to store the amount of interest being generated
+    loan_interest_dict = {}
+    
+    # the amount of money allocated to pay off debt
+    debt_monthly_budgeted = amount
 
+    # how many loans are in our debt_df
+    loan_count = len(debt_df)
+
+    # how many active loans are in debt_df
+    active_loan_count = 0
+
+    # checking for paid off loans
+    for row in range(loan_count):
+        if debt_df.iloc[row]["balance"] <= 0:
+            debt_df.iloc[row, 1] = 0
+        else:
+            active_loan_count += 1
+            #return 
+
+    # for each loan add the interest to the loan
+    for row in range(loan_count):    
+        # for each loan, calculate the interest owed in dollars
+        interest = float(debt_df.iloc[row]["balance"] * debt_df.iloc[row]["monthly_interest_rate"])
+        
+        #print("interest:", interest)
+        # add the owed interest to the dictionary
+        loan_interest_dict[debt_df.iloc[row]["name"]] = interest
+    #return
+    # Calculate which loans are generating the most interest
+    loan_interest_total = sum(loan_interest_dict.values())
+    loan_interest_percent_dict = {}
+    # calculate the relative percentages of the loans
+    for loan in loan_interest_dict:
+        loan_interest_percent_dict[loan] = ( loan_interest_dict[loan] / loan_interest_total)
+
+
+    print("should be 100:",sum(loan_interest_percent_dict.values()))
+    # Simulate paying off the loan
+    extra = 0
+    total_debt = debt_df['balance'].sum()
+    extra = debt_monthly_budgeted - total_debt
+    
+
+    for loan in loan_interest_dict:
+        # find the row in the dataframe that matches the current loan
+        
+        df_row = debt_df.loc[debt_df['name'] == loan]
+        
+        index = debt_df.loc[debt_df['name'] == loan].index[0]
+        balance = df_row['balance']
+        balance = float(balance.iloc[0])
+        if extra > 0:
+            print("sum of debt", sum(loan_interest_dict.values()))
+            print(total_debt)
+            # print("wtf")
+            payment = total_debt * loan_interest_percent_dict[loan]
+        else:
+            payment = debt_monthly_budgeted * loan_interest_percent_dict[loan]
+
+        # print("debt amount:", debt_df.iloc[index, 1])
+        # print("amount paid:", payment)
+        
+        debt_balance = (balance + loan_interest_dict[loan]) - payment
+        debt_df.iloc[index, 1] = debt_balance
+    return debt_df, extra
+#debt_calculation = calculate_debt_after_period(PERIOD, debt_df, standard_budgeting_percentage_dict)
+
+
+
+
+
+
+
+
+for i in range(WIGGLE + 1):
+# TODO handle different budget configurations    
+
+    # main loop
+    for year in range(PERIOD):
+        # I don't like having the year start at 0
+        year = year + 1
+
+        for month in range(12):
+            # I don't like having the month start at 0
+            month = month + 1
+            this_months_salary = monthly_salary
+            this_months_budget = calculate_dollars_from_percentage(this_months_salary, real_budgeting_percentage_dict)
+            debt_budget = this_months_budget["debt"]
+            investments_budget = this_months_budget["investments"]
+            debt_df, extra_funds = pay_debt(debt_df, 500)
+            #print(debt_df)
+
+            print("year", year, "month", month, "\n", debt_df)
+
+            #print(this_months_budget)
+            #print(month)
+            #print(standard_monthly_dollar_budget)
